e832f1f23dd31c548da2b2a5320d2a2bed20c2cd
Anderson0017 <andeerson0017@gmail.com>
2020-03-08 21:18:51 -0300

    Adicionando os imports

--- src/screens/AddTasks.js
+++ src/screens/AddTasks.js
@@ -1,4 +1,13 @@
 import React , {Component} from 'react'
 import {
-
+    Modal,
+    View,
+    Text,
+    TextInput,
+    DatePickerIOS,
+    StyleSheet,
+    TouchableWithoutFeedback,
+    TouchableOpacity
 } from 'react-native'
+import moment from 'moment'
+import commonStyle from '../commonStyle'
